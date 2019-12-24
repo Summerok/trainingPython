51e19bf88680b7244fab94b01621b2c08f23d544
Summerok <abbey@msn.com>
2019-12-24 08:12:48 +0100

    adding new functionalities to test_cases

--- test-workspace/test_cases.py
+++ test-workspace/test_cases.py
@@ -5,29 +5,33 @@ import unittest
 class RandomTest(unittest.TestCase):
 
     """Test case utilisé pour tester les fonctions du module 'random'."""
+    def setUp(self):
+        """Initialisation des tests"""
+        self.liste = list(range(10))
 
     def test_choice(self):
         """Test le fonctionnement de la fonction 'random.choice'."""
-        liste = list(range(10))
-        elt = random.choice(liste)
-        self.assertIn(elt, liste)
+        elt = random.choice(self.liste)
+        self.assertIn(elt, self.liste)
     
     def test_shuffle(self):
         """Test le fonctionnement de la fonction 'random.shuffle'."""
-        liste = list(range(10))
-        random.shuffle(liste)
-        liste.sort()
-        self.assertEqual(liste, list(range(10)))
+        random.shuffle(self.liste)
+        self.liste.sort()
+        self.assertEqual(self.liste, list(range(10)))
         
     def test_sample(self):
         """Teste le fonctionnement de la fonction 'random.sample'."""
-        liste = list(range(10))
-        extrait = random.sample(liste, 9)
+        extrait = random.sample(self.liste, 9)
         for elt in extrait:
             self.assertIn(elt, extrait)
         
-        self.assertRaises(ValueError, random.sample, liste, 20)
+        self.assertRaises(ValueError, random.sample, self.liste, 20)
+        with self.assertRaises(ValueError):
+            random.sample(self.liste,20)
 
+    def tearDown(self):
+        """Finalisation du test"""
+        print("Fin du test")
 
-
-unittest.main()        
+      
